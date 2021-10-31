b4aacd163a818a9b648627801b609b6256dd6ba1
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-10-31 00:10:34 -0400

    yes

---
+++ models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    creator: String,
+    name: String,
+    number: String,
+    amount: String,
+    tags: [String],
+    selectedFile: String,
+    // applyNow: {
+    //     type: Number,
+    //     default: 0
+    // },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+
+});
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
