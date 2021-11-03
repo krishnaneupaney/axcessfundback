9099e0d51b0c514544f12d8329a2df19d80f3e0a
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-11-02 23:49:17 -0400

    ooak

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
