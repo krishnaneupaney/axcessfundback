a308070d3d0a77ba8590f6b2bdf19d0e0c1306af
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-11-02 23:42:08 -0400

    oka

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
